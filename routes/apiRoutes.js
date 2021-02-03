c8100b6af7e67d99bd8eaeab8c1649bfa7c0234a
Rebecca Chiquete <deerinra194@gmail.com>
2021-02-03 11:17:43 -0700

    pushing

---
+++ routes/apiRoutes.js
@@ -0,0 +1,42 @@
+var notes = require("../db/db.json");
+var fs = require("fs");
+var { v4: uuidv4 } = require('uuid');
+
+module.exports = function (app) {
+  app.get("/api/notes", function (req, res) {
+    res.json(notes);
+  });
+
+  app.post("/api/notes", function (req, res) {
+    // add an id to the req.body before it gets pushed in
+    req.body.id = uuidv4();
+    // then push it into notes
+    notes.push(req.body);
+
+    fs.writeFile("./db/db.json", JSON.stringify(notes), "utf8", function (err) {
+      if (err) throw err;
+      res.json(req.body);
+    })
+
+  });
+
+  
+  app.delete("/api/notes/:id", function (req, res) {
+    var id = req.params.id;
+    console.log(id);
+    console.log(notes);
+
+    // find a way to delete from the notes array using the id
+    // filter or for loop
+
+
+    console.log(notes);
+    fs.writeFile("./db/db.json", JSON.stringify(notes), "utf8", function (err) {
+      if (err) throw err;
+      res.json(req.body);
+    })
+
+  });
+
+
+};
